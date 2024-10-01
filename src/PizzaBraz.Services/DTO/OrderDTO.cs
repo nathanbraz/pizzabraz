f40f3990fcd18627866d987ad3d8fd963a5eb769
Nathan <nathanbrz09@gmail.com>
2024-10-01 13:42:57 -0300

    Interface de Order criada

---
+++ src/PizzaBraz.Services/DTO/OrderDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBraz.Services.DTO
+{
+    public class OrderDTO
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid CompanyId { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
---
+++ src/PizzaBraz.Services/Interfaces/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using PizzaBraz.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBraz.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<OrderDTO> Create(OrderDTO orderDTO);
+        Task<OrderDTO> Update(OrderDTO orderDTO);
+        Task Remove(Guid id);
+        Task<OrderDTO> Get(Guid id);
+    }
+}
